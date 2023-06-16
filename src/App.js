b86cc20d0179a03a3dbac858273933eab6f710b9
Samuel Cornet <corsam28@gmail.com>
2023-06-16 01:11:11 -0500

    Review the useREffect hook

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import logo from './logo.svg';
 import './App.css';
 import UseStateHook from './UseStateHook';
 import UseReducerHook from './UseReducerHook';
+import UseEffectHook from './UseEffectHook';
 
 function App() {
   return (
     <div className="App">
       {/* <UseStateHook />  */}
-      <UseReducerHook />
+      {/* <UseReducerHook /> */}
+      <UseEffectHook />
     </div>
   );
 }
